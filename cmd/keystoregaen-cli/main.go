52067f84abaeea4e2a2be0f52515f034c7d7b3ff
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-10-24 22:39:53 +0200

    feat: Connect keystore generator to Wasm app

--- cmd/keystoregaen-cli/main.go
+++ cmd/keystoregaen-cli/main.go
@@ -1,18 +1,13 @@
 package main
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"crypto/x509/pkix"
 	"flag"
-	"math"
-	"math/big"
+	"log"
 	"os"
 	"strings"
 	"time"
 
-	ks "github.com/pavlo-v-chernykh/keystore-go/v4"
+	"github.com/pojntfx/keystoregaen/pkg/utils"
 )
 
 func main() {
@@ -21,7 +16,7 @@ func main() {
 	alias := flag.String("alias", "", "Alias of the certificate to generate")
 	cname := flag.String("cname", "", "CNAME of the certificate to generate")
 	validity := flag.Duration("validity", time.Hour*24*365, "Validity of certificate to generate")
-	bits := flag.Int("bits", 2048, "Bits to use to generate RSA key")
+	bits := flag.Uint("bits", 2048, "Bits to use to generate RSA key")
 	dst := flag.String("dst", "keystoregaen.jks", "Path to write the keystore to")
 
 	flag.Parse()
@@ -42,68 +37,23 @@ func main() {
 		panic("could not continue with empty CNAME")
 	}
 
-	// Generate private key
-	key, err := rsa.GenerateKey(rand.Reader, *bits)
-	if err != nil {
-		panic(err)
-	}
-
-	rawKey, err := x509.MarshalPKCS8PrivateKey(key)
-	if err != nil {
-		panic(err)
-	}
-
-	// Generate certificate
-	serialNumber, err := rand.Int(rand.Reader, big.NewInt(math.MaxInt64))
-	if err != nil {
-		panic(err)
-	}
-
-	now := time.Now()
-	tpl := &x509.Certificate{
-		SerialNumber: serialNumber,
-		NotBefore:    now,
-		NotAfter:     now.Add(*validity),
-		Subject: pkix.Name{
-			CommonName: *cname,
-		},
-		Issuer: pkix.Name{
-			CommonName: *cname,
-		},
-	}
-
-	cert, err := x509.CreateCertificate(rand.Reader, tpl, tpl, &key.PublicKey, key)
-	if err != nil {
-		panic(err)
-	}
-
-	// Generate & write keystore
-	keystore := ks.New()
-
-	if err := keystore.SetPrivateKeyEntry(
-		*alias,
-		ks.PrivateKeyEntry{
-			CreationTime: time.Now(),
-			PrivateKey:   rawKey,
-			CertificateChain: []ks.Certificate{
-				{
-					Type:    "X509",
-					Content: cert,
-				},
-			},
-		},
-		[]byte(*keypass),
-	); err != nil {
-		panic(err)
-	}
-
 	out, err := os.Create(*dst)
 	if err != nil {
 		panic(err)
 	}
 	defer out.Close()
 
-	if err := keystore.Store(out, []byte(*storepass)); err != nil {
+	log.Println("Generating keystore ...")
+
+	if err := utils.GenerateKeystore(
+		*storepass,
+		*keypass,
+		*alias,
+		*cname,
+		*validity,
+		uint32(*bits),
+		out,
+	); err != nil {
 		panic(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,18 @@ go 1.18
 require (
 	github.com/maxence-charriere/go-app/v9 v9.6.7
 	github.com/pavlo-v-chernykh/keystore-go/v4 v4.4.0
+	github.com/pojntfx/keygaen v0.1.0
 )
 
-require github.com/google/uuid v1.3.0 // indirect
+require (
+	github.com/ProtonMail/go-crypto v0.0.0-20210920160938-87db9fbc61c7 // indirect
+	github.com/ProtonMail/go-mime v0.0.0-20190923161245-9b5a4261663a // indirect
+	github.com/ProtonMail/gopenpgp/v2 v2.2.4 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.4.2 // indirect
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
