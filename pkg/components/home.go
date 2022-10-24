5c9df87df4e73c125d04ecc7bf37910311acfb84
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-10-24 22:39:53 +0200

    feat: Connect keystore generator to Wasm app

--- pkg/components/home.go
+++ pkg/components/home.go
@@ -1,13 +1,19 @@
 package components
 
 import (
+	"bytes"
 	"log"
+	"time"
 
 	"github.com/maxence-charriere/go-app/v9/pkg/app"
+	"github.com/pojntfx/keygaen/pkg/components"
+	"github.com/pojntfx/keystoregaen/pkg/utils"
 )
 
 type Home struct {
 	app.Compo
+
+	err error
 }
 
 func (c *Home) Render() app.UI {
@@ -31,12 +37,63 @@ func (c *Home) Render() app.UI {
 						Body(
 							&KeystoreGenerationForm{
 								OnSubmit: func(storepass, keypass, alias, cname string, validity, bits uint32) {
-									log.Println(storepass, keypass, alias, cname, validity, bits)
+									out := bytes.NewBuffer([]byte{})
+
+									go func() {
+										log.Println("Generating keystore ...")
+
+										if err := utils.GenerateKeystore(
+											storepass,
+											keypass,
+											alias,
+											cname,
+											time.Hour*24*time.Duration(validity),
+											bits,
+											out,
+										); err != nil {
+											c.panic(err)
+
+											return
+										}
+
+										c.download(out.Bytes(), "keystoregaen.jks", "application/octet-stream")
+									}()
 								},
 							},
 						),
 				),
+			app.If(
+				c.err != nil,
+				&components.ErrorModal{
+					ID:          "error-modal",
+					Icon:        "fas fa-times",
+					Title:       "An Error Occurred",
+					Class:       "pf-m-danger",
+					Body:        "The following details may be of help:",
+					Error:       c.err,
+					ActionLabel: "Close",
+
+					OnClose: func() {
+						c.recover()
+					},
+					OnAction: func() {
+						c.recover()
+					},
+				},
+			),
 		)
+}
+
+func (c *Home) panic(err error) {
+	log.Println(err)
+
+	c.err = err
+
+	c.Update()
+}
+
+func (c *Home) recover() {
+	c.err = nil
 }
 
 func (c *Home) OnAppUpdate(ctx app.Context) {
